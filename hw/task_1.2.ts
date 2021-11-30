ad0f82eb4156bbb0c5779db9a91e164e9e1113c6
Aleksandr Konovalov <aleksandr_konovalov@epam.com>
2021-11-30 21:31:29 +0300

    :hammer: Refactor task_1.2

--- hw/task_1.2.ts
+++ hw/task_1.2.ts
@@ -1,42 +1,45 @@
 import path from 'path';
 import { createReadStream, createWriteStream } from 'fs';
 import csvToJson from 'csvtojson/v2';
-import { Writable, WritableOptions } from 'stream';
-import { StringDecoder } from 'string_decoder';
-
-class StringWritable extends Writable {
-    _decoder: StringDecoder;
-    data: string;
-
-    constructor(options: WritableOptions) {
-        super(options);
-        this._decoder = new StringDecoder(options.defaultEncoding);
-        this.data = '';
-    }
-
-    _write(chunk: Buffer | string, encoding: string, callback: () => void) {
-        if (encoding === 'buffer') {
-            chunk = this._decoder.write(chunk as Buffer);
-        }
-        this.data += chunk;
-        console.log(this.data);
-        callback();
-    }
-
-    _final(callback: () => void) {
-        this.data += this._decoder.end();
-        callback();
-    }
-}
+import { pipeline, Transform } from 'stream';
 
 const inputFilePath = path.join(__dirname, 'csv', 'input.csv');
 const outputFilePath = path.resolve(__dirname, 'csv', 'output.txt');
 
 const inputStream = createReadStream(inputFilePath);
-const outputStream = createWriteStream(outputFilePath, { highWaterMark: 2 });
-const dbStream = new StringWritable({
-    highWaterMark: 1,
-    defaultEncoding: 'utf8',
+const outputStream = createWriteStream(outputFilePath);
+const dbStream = new Transform({
+    transform: function (this, chunk, _encoding, callback) {
+        this.push(chunk);
+        new Promise((resolve) => {
+            setTimeout(resolve, 300);
+        });
+        callback();
+    },
+});
+
+type BookInfo = {
+    Book: string;
+    Author: string;
+    Amount: string;
+    Price: string;
+};
+const transformStream = new Transform({
+    transform: function (this, chunk, _encoding, callback) {
+        try {
+            const bookInfo: BookInfo = JSON.parse(chunk.toString());
+            const transformedChunk = `${JSON.stringify({
+                book: bookInfo.Book,
+                author: bookInfo.Author,
+                price: bookInfo.Price,
+            })}\n`;
+            this.push(transformedChunk);
+            callback();
+        } catch (e) {
+            this.push(chunk);
+            callback();
+        }
+    },
 });
 
 inputStream
@@ -46,6 +49,7 @@ inputStream
     .on('end', () => {
         outputStream.end();
         dbStream.end();
+        transformStream.end();
     });
 
 outputStream.on('error', (e) => {
@@ -56,39 +60,12 @@ dbStream.on('error', (e) => {
     console.error(e);
 });
 
-type Book = {
-    Book: string;
-    Author: string;
-    Amount: string;
-    Price: string;
-};
+transformStream.on('error', (e) => {
+    console.error(e);
+});
 
-csvToJson()
-    .fromStream(inputStream)
-    .subscribe(
-        (book: Book) => {
-            return new Promise((resolve, reject) => {
-                const line = `${JSON.stringify(book)}\n`;
-
-                outputStream.write(line, (e) => {
-                    if (e) {
-                        console.error(e);
-
-                        reject(e);
-                    }
-                });
-                dbStream.write(line, (e) => {
-                    if (e) {
-                        console.error(e);
-
-                        reject(e);
-                    }
-                });
-
-                resolve();
-            });
-        },
-        (e) => {
-            console.error(e);
-        },
-    );
+pipeline(inputStream, csvToJson(), transformStream, dbStream, outputStream, (e) => {
+    if (e) {
+        console.error(e);
+    }
+});
